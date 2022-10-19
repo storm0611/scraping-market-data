37e2964ed3b760495d54fa41b6de24f7ac27fb73
devstar0611 <103535364+devstar0611@users.noreply.github.com>
2022-10-20 05:02:57 +0900

    final

--- main.py
+++ main.py
@@ -21,37 +21,42 @@ def get_name_by_id(id: str):
         #                         chrome_options=chrome_options)
         try:
             driver = webdriver.Chrome()
+            driver.execute_script("window.open('');")
+            driver.switch_to.window(driver.window_handles[-1])
+            
+            driver.get("https://www.manpower.gov.kw/Pages/Services/TrainingRegister.aspx")
+
+            driver.execute_script("document.body.style.zoom='100%'")
+            
+            id_element = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#ContentPlaceHolder1_TrainingRegisterUC_txtCivilID")))
+            action = ActionChains(driver) 
+            action.move_to_element(id_element).click().perform()
+            id_element.send_keys(id)
+            time.sleep(1)
+            action.send_keys(Keys.ENTER).perform()
+
+            name_element = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#ContentPlaceHolder1_TrainingRegisterUC_lblName")))
+            name = name_element.text
+            driver.close()
+            driver.quit()
+            return name
         except:
             os.system("taskkill /im chrome.exe /f")
-        driver.execute_script("window.open('');")
-        driver.switch_to.window(driver.window_handles[-1])
-        
-        driver.get("https://www.manpower.gov.kw/Pages/Services/TrainingRegister.aspx")
-
-        driver.execute_script("document.body.style.zoom='100%'")
-        
-        id_element = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#ContentPlaceHolder1_TrainingRegisterUC_txtCivilID")))
-        action = ActionChains(driver) 
-        action.move_to_element(id_element).click().perform()
-        id_element.send_keys(id)
-        time.sleep(1)
-        action.send_keys(Keys.ENTER).perform()
-
-        name_element = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#ContentPlaceHolder1_TrainingRegisterUC_lblName")))
-        name = name_element.text
-        driver.close()
-        driver.quit()
-        return name
-
+            return "repeat"
 
 if __name__ == "__main__":
     df = pd.read_excel("id.xlsx", sheet_name="Sheet1")
     list_id = []
     list_name = []
-    for id in df['ID']:
-        name = get_name_by_id(str(id))
-        time.sleep(3)
-        list_id.append(str(id))
-        list_name.append(str(name))
+    id = 0
+    while id < len(df):
+        name = get_name_by_id(str(df.at[id, 'ID']))
+        if 'repeat' in name:
+            print("error in chrome driver")
+        else:
+            time.sleep(3)
+            list_id.append(str(df.at[id, 'ID']))
+            list_name.append(str(name))
+        id += 1
     df = DataFrame(list(zip(list_id, list_name)), columns=['ID', 'NAME'])
     df.to_excel("name.xlsx", sheet_name='Sheet1')
