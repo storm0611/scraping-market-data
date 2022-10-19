f2ae916f4515c25867e8adc1152ecb7672fc2358
devstar0611 <103535364+devstar0611@users.noreply.github.com>
2022-10-19 09:24:17 +0900

    encode

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def get_name_by_id(id: str):
         action.send_keys(Keys.ENTER).perform()
 
         name_element = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#ContentPlaceHolder1_TrainingRegisterUC_lblName")))
-        name = str(name_element.text.encode("utf-8"))
+        name = str(name_element.text.encode("unicode"))
         driver.close()
         driver.quit()
         return name
