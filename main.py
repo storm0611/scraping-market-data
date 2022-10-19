58d1d80906391bf505810a3434b0131c57f63e90
devstar0611 <103535364+devstar0611@users.noreply.github.com>
2022-10-19 10:10:49 +0900

    p

--- main.py
+++ main.py
@@ -44,5 +44,5 @@ if __name__ == "__main__":
     file = open("output.txt", "w", encoding="utf8")
     name = get_name_by_id("301092401424")
     file.write("id = {0} => name = ".format("301092401424") + name)
-    print("id = {0} => name = ".format("301092401424"), str(name))
+    # print("id = {0} => name = ".format("301092401424"), str(name))
     file.close()
